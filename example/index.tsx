24076d3ae1d73ffcc6c409c3aab147fddd8aa211
stiven <adisjimenez@misena.edu.co>
2022-12-21 16:44:23 -0500

    Version 1.0.0rc Lista

--- example/index.tsx
+++ example/index.tsx
@@ -6,7 +6,7 @@ import { ProductCard, ProductImage, ProductTitle, ProductButtons } from '../.';
 const product = {
   id: '1',
   title: 'Coffee Mug - Card',
-  img: './coffee-mug.png',
+  img: './coffee-mug.png!',
 };
 
 const App = () => {
